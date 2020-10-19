a3d41b5a70d1c52fd9763e06f47ccc46dcd86563
Your Name <you@example.com>
2020-10-19 15:44:43 -0400

    Working encoderreader with Rasbpi

--- encoderreader.py
+++ encoderreader.py
@@ -1,56 +1,34 @@
 import time
 import RPi.GPIO as GPIO
-from multiprocessing import Process
-from settings import ENCODER_PIN, ENCODER_REV_PULSE, ENCODER_READ_INTERVAL, WHEEL_SIZE, RAIL_LENGTH
+from multiprocessing import Value
+from settings import ENCODER_PIN_A, ENCODER_PIN_B, ENCODER_REV_PULSE, ENCODER_READ_INTERVAL, WHEEL_SIZE, RAIL_LENGTH
 
 
-class EncoderReader(Process):
+class EncoderReader():
 
-    def __init__(self, position, distance, forward, lock):
-        super().__init__()
-        self.daemon = True
+    def __init__(self, position, distance):
         self.position = position
         self.distance = distance
-        self.forward = forward
-        self.lock = lock
-        self.pulses = 0
+        self.distancePerPulse = (1 / ENCODER_REV_PULSE) * WHEEL_SIZE
+        self.positionPerPulse = self.distancePerPulse / RAIL_LENGTH
         self.giop_setup()
-
 
     def giop_setup(self):
         print('Setting up encoder...')
         try:
-            GPIO.setup(ENCODER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(ENCODER_PIN, GPIO.RISING, callback=self.pulse_counter)
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(ENCODER_PIN_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(ENCODER_PIN_A, GPIO.RISING, callback=self.pulse_interpreter)
+            GPIO.setup(ENCODER_PIN_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(ENCODER_PIN_B, GPIO.RISING, callback=self.pulse_interpreter)
         except Exception as e:
             print('Failed to set up encoder:')
             raise e
         else:
             print('Encoder set up!')
     
-
-    def pulse_counter(self):
-	    self.pulses += 1
-    
-
-    def run(self):
-
-        currentSecs = 0
-        previousSecs = 0
-
-        while True:
-            currentSecs = time.time()
-            if (currentSecs - previousSecs) > ENCODER_READ_INTERVAL:
-                previousSecs = currentSecs
-                pulsesSince = self.pulses
-                self.pulses = 0
-                distanceSince = (pulsesSince / ENCODER_REV_PULSE) * WHEEL_SIZE
-                positionSince = distanceSince / RAIL_LENGTH
-                with self.lock:
-                    self.distance.value += distanceSince
-                    self.position.value += positionSince
-    
-
-    def terminate(self):
-        GPIO.cleanup()
-        super().terminate()
+    def pulse_interpreter(self, _):
+        with self.position.get_lock():
+            self.position.value += self.positionPerPulse
+        with self.distance.get_lock():
+            self.distance.value += self.distancePerPulse
