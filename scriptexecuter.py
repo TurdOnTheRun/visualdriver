00359be113f3afd4d5182fba62892cdf0742dd03
Your Name <you@example.com>
2020-11-08 14:34:45 -0500

    gogogo

--- scriptexecuter.py
+++ scriptexecuter.py
@@ -1,8 +1,8 @@
 from arduinopwmmanager import ArduinoPwmManager
 from encoderreader import EncoderReader
 from multiprocessing import Value, Queue
-from settings import ARDUINO_UNO_CONN
-from whiteroom import COMMANDS
+from settings import ARDUINO_UNO_CONN, ARDUINO_MEGA_CONN
+from humanposerecognition import COMMANDS
 import time
 import atexit
 
@@ -19,67 +19,68 @@ bottom.start()
 
 
 def execute_command(element,value):
-	if element == 'motor':
-		bottomComm.put('5'+value)
-		# print('bottom', '5'+value)
-	else:
-		lightid = element.split('-')[1]
-		if element.startswith('top'):
-			topComm.put(lightid + value)
-			# print('top', lightid + value)
-		else:
-			bottomComm.put(lightid + value)
-			# print('bottom', lightid + value)
+    if element == 'motor':
+        bottomComm.put(int('5'+value))
+        # print('bottom', '5'+value)
+    else:
+        lightid = element.split('-')[1]
+        if element.startswith('top'):
+            topComm.put(int(lightid + value))
+            # print('top', lightid + value)
+        else:
+            bottomComm.put(int(lightid + value))
+            # print('bottom', lightid + value)
 
 
 def setup():
-	bottomComm.put('5000')
-	topComm.put('1001')
-	topComm.put('2001')
-	topComm.put('5001')
-	topComm.put('6001')
-	bottomComm.put('1000')
-	bottomComm.put('2000')
+    bottomComm.put(5000)
+    topComm.put(1001)
+    topComm.put(2001)
+    topComm.put(5001)
+    topComm.put(6001)
+    bottomComm.put(1000)
+    bottomComm.put(2000)
 
 
 def shutdown():
-	bottomComm.put('5000')
-	topComm.put('1200')
-	topComm.put('2200')
-	topComm.put('5200')
-	topComm.put('6200')
-	bottomComm.put('1200')
-	bottomComm.put('2200')
+    bottomComm.put(5000)
+    topComm.put(1200)
+    topComm.put(2200)
+    topComm.put(5200)
+    topComm.put(6200)
+    bottomComm.put(1200)
+    bottomComm.put(2200)
 
 
 atexit.register(shutdown)
 
 
-setup()
+#setup()
 time.sleep(15)
 
 
 try:
-	last = time.time()
-	i = 0
-	while i < len(COMMANDS):
-		command = COMMANDS[i]
-		if command[0] == 'time':
-			if time.time() - last > command[1]:
-				for x in command[2:]:
-					execute_command(x[0],x[1])
-				i+=1
-		elif command[0] == 'pos':
-			if position.value > command[1]:
-				for x in command[2:]:
-					execute_command(x[0],x[1])
-				i+=1
-		elif command[0] == 'resettimer':
-			last = time.time()
-			i+=1
+    last = time.time()
+    i = 0
+    while i < len(COMMANDS):
+        command = COMMANDS[i]
+        if command[0] == 'time':
+            if time.time() - last > command[1]:
+                for x in command[2:]:
+                    execute_command(x[0],x[1])
+                i+=1
+        elif command[0] == 'pos':
+            if position.value > command[1]:
+                for x in command[2:]:
+                    execute_command(x[0],x[1])
+                i+=1
+        elif command[0] == 'resettimer':
+            last = time.time()
+            i+=1
 except Exception:
-	shutdown()
+    shutdown()
 
+shutdown()
 
-		
+        
 
