57d84a590312b7a0fcf80ed66dc9defc2a06e747
Maximilian <iviaximilianweber@gmail.com>
2020-11-24 20:56:28 +0100

    tester

--- tests/arduino_sunshine_tester.py
+++ tests/arduino_sunshine_tester.py
@@ -5,18 +5,29 @@ time_now = lambda: int(round(time.time() * 1000))
 
 BAUDRATE = 115200
 
-# ser = serial.Serial( "/dev/cu.HC-06-SPPDev", baudrate=BAUDRATE )
-ser = serial.Serial( "/dev/cu.usbmodem1431", baudrate=115200 , timeout=1)
+ser = serial.Serial( "/dev/cu.HC-06-SPPDev", baudrate=BAUDRATE )
+# ser = serial.Serial( "/dev/cu.usbmodem1431", baudrate=115200 , timeout=1)
 
-def send_it(id, state, tostate, steptime):
-	print(id, state, tostate, steptime)
-	command = b'<' + bytes([id]) + bytes([state]) + bytes([tostate]) + bytes([steptime]) + b'>\n'
+def send_it(id, state, steptime):
+	print(id, state, steptime)
+	if steptime:
+		command = b'<' + bytes([id]) + bytes([state]) + bytes([steptime]) + b'>\n'
+	else:
+		command = b'<' + bytes([id]) + bytes([state]) + b'>\n'
 	ser.write(command)
 
 while True:
     value = input("Please enter a command:\n")
     try:
         commands = value.split(',')
-        send_it(int(commands[0]), int(commands[1]), int(commands[2]), int(commands[3]))
-    except Exception:
+        id = int(commands[0])
+        if len(commands) > 1:
+        	state = int(commands[1])
+        if len(commands) > 2:
+        	steptime = int(commands[2])
+        else:
+        	steptime = None
+        send_it(id, state, steptime)
+    except Exception as e:
         print('Invalid Command')
+        raise e
---
+++ tests/uma.py
@@ -0,0 +1,13 @@
+BASE_BRIGHTNESS = 50
+
+SCRIPT = [
+	['time',2.3,['bottom',[200,60]]],
+	['time',39.7,['top',[101,200,4]]],
+	['time',40.32,['top',[101,BASE_BRIGHTNESS,17]]],
+	['time',43.8,['top',[101,200,4]]],
+	['time',44.5,['top',[101,BASE_BRIGHTNESS,17]]],
+	['time',47.6,['top',[101,200,4]]],
+	['time',48.3,['top',[101,BASE_BRIGHTNESS,17]]],
+	['time',51.6,['top',[101,200,4]]],
+	['time',52.2,['top',[101,BASE_BRIGHTNESS,17]]],
+]
