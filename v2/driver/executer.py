a7683ead20c96761b82f89a3f54b99f59b7b4b2d
Maximilian Weber <iviaximilianweber@gmail.com>
2022-01-14 16:56:58 +0100

    meyron shoot

--- v2/driver/executer.py
+++ v2/driver/executer.py
@@ -38,8 +38,9 @@ from settings import ARDUINO_UNO_CONN, ARDUINO_MEGA_CONN, SONY_TRIGGER
 
 # buildUp = thatSpatialEvolvingFuzz([(0.1,0.025), (0.1,0.025), (0.1,0.05)], 5, (75, 90), (20,25), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100)])
 
-buildUp = thatSpatialEvolvingFuzz([(0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.035)], 5, (40, 50), (10,20), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100)])
-bigBumps = thatSpatialEvolvingFuzz([(0.3, 0.035), (0.3, 0.035), (0.3, 0)], 8, (110, 130), (40,50), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100)])
+# buildUp = thatSpatialEvolvingFuzz([(0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.025), (0.1,0.035)], 5, (40, 50), (10,20), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100)])
+buildUp = thatSpatialEvolvingFuzz([(0.15,0.025), (0.15,0.025), (0.15,0.025), (0.15,0.025), (0.15,0.025), (0.15,0.025), (0.15,0.035)], 5, (40, 50), (10,20), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100)])
+bigBumps = thatSpatialEvolvingFuzz([(0.3, 0.035), (0.3, 0.035), (0.3, 0)], 8, (40, 50), (10,20), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100)])
 eventDict = buildUp
 eventDict['position'] += bigBumps['position']
 eventDict['time'] += bigBumps['time']
