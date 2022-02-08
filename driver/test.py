63f0e249af8e410a74a8a9076db14700faa6ac28
Maximilian Weber <maximilian@sudo.site>
2022-02-08 18:27:10 +0100

    new settings

--- driver/test.py
+++ driver/test.py
@@ -1,12 +1,13 @@
 from arduinopwmmanager import ArduinoPwmManager
 from encoderreader import EncoderReader
 from multiprocessing import Value, Queue
-from settings import ARDUINO_MEGA_CONN, ARDUINO_UNO_CONN, SONY_TRIGGER
+from settings import ARDUINO_MEGA_CONN, ARDUINO_UNO_CONN
 
 pwmComm = Queue()
 position = Value('d', 0.0)
 distance = Value('d', 0.0)
 er = EncoderReader(position, distance)
+er.start()
 
 pwm = ArduinoPwmManager(ARDUINO_UNO_CONN, pwmComm)
 pwm.start()
