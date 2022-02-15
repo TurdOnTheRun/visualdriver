018c20120b3a1a8db3518fa04d3286ad027f1116
Maximilian Weber <maximilian@sudo.site>
2022-02-16 00:48:20 +0100

    denis scripts and env

---
+++ scripts/denis.py
@@ -0,0 +1,2 @@
+eventDict = sideToSideTwo(30, (Bottom1, 100, 100), (TopAll, 90, 40), (Bottom2, 100, 100), 1)
++eventDict = sideToSideTwo(60, (Bottom1, 100, 200), (TopAll, 90, 80), (Bottom2, 100, 200), 2)
