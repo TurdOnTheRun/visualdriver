9c16d08a3aff98365bc20495bcb8faefbd2eba4e
Maximilian Weber <iviaximilianweber@gmail.com>
2022-02-16 20:29:04 +0100

    iterations added to flashCollection

--- driver/executer.py
+++ driver/executer.py
@@ -37,6 +37,9 @@ from photoevents import flashCollection
 # ISO 100, 16
 eventDict = flashCollection([(Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Top1, 100, 250), (Top1, 100, 250)])
 
+# ISO 100, 11-18
+eventDict = flashCollection([(Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Bottom1, 100, 250), (Bottom2, 100, 250), (Top1, 100, 250), (Top1, 100, 250)], 3)
+
 
 timeEvents = eventDict.get('time', [])
 positionEvents = eventDict.get('position', [])
--- driver/photoevents.py
+++ driver/photoevents.py
@@ -2,18 +2,26 @@ from events import *
 from settings import TRIGGER_SONY_DOWN, TRIGGER_SONY_UP
 
 
-def flashCollection(agentsStatesDurations):
+def flashCollection(agentsStatesDurations, iterations=1):
 
     eventDict = {
         'time': [TriggerAngle(At(0), TRIGGER_SONY_DOWN),]
     }
 
     currentTime = 0.5
+    iterationDuration = None
 
-    for agent, state, duration in agentsStatesDurations:
-        eventDict['time'].append(Flash(At(currentTime), agent, state, duration))
-        currentTime += 1
-    
-    eventDict['time'].append(TriggerAngle(At(0), TRIGGER_SONY_UP))
+    for i in range(iterations):
+        for agent, state, duration in agentsStatesDurations:
+            eventDict['time'].append(Flash(At(currentTime), agent, state, duration))
+            currentTime += 1
+        
+        eventDict['time'].append(TriggerAngle(At(currentTime), TRIGGER_SONY_UP))
+
+        if not iterationDuration:
+            iterationDuration = currentTime
+        
+        currentTime += iterationDuration
+
 
     return eventDict
