7f0bdc739f214c6e2c06a9ce76e00bd79158186d
Maximilian Weber <iviaximilianweber@gmail.com>
2022-03-05 18:12:07 +0100

    fuzz

--- driver/executer.py
+++ driver/executer.py
@@ -19,8 +19,16 @@ eventDict = {
     'time': []
 }
 
+eventDict = thatFuzz(49, (40, 50), (10,20), ((Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100), (BottomAll, 80)))
+eventDict['position'] = [MotorSpeed(At(0), 110, 30), TimeEventsUnblock(At(0.5))] + eventDict['position']
+eventDict['time'] = [TimeEventsBlock(At(0)),] + eventDict['time']
 
-eventDict = dancingInTheVoid(29, (60,65), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100), (BottomAll, 80)], motorspeed=100, accelerationArc=0.5)
+# eventDict = dancingInTheVoid(58, (248,255), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100), (BottomAll, 80)], motorspeed=100, accelerationArc=0.5)
+
+
+
+
+
 
 # eventDict = schattentanzRandomBezier(100, 3, 10, [(TopAll, 100), (BottomAll, 100)], 3, 255, accelerationArc=0.5)
 
