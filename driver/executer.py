54175ba244ee03f4c2be77735191a77ac7b20444
Maximilian Weber <iviaximilianweber@gmail.com>
2022-03-05 18:15:03 +0100

    history

--- driver/executer.py
+++ driver/executer.py
@@ -20,10 +20,10 @@ eventDict = {
 }
 
 eventDict = thatFuzz(49, (40, 50), (10,20), ((Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100), (BottomAll, 80)))
-eventDict['position'] = [MotorSpeed(At(0), 110, 30), TimeEventsUnblock(At(0.5))] + eventDict['position']
+eventDict['position'] = [MotorSpeed(At(0), 111, 30), TimeEventsUnblock(At(0.5))] + eventDict['position']
 eventDict['time'] = [TimeEventsBlock(At(0)),] + eventDict['time']
 
-# eventDict = dancingInTheVoid(58, (248,255), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100), (BottomAll, 80)], motorspeed=100, accelerationArc=0.5)
+# eventDict = dancingInTheVoid(58, (200,220), [(Top1, 100), (Top2, 100), (Top3, 100), (Top4, 100), (BottomAll, 80)], motorspeed=111, accelerationArc=0.5)
 
 
 
