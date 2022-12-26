84ea5ef3c8b205d3cf64587ff6a0788cc18802e8
Maximilian Weber <maximilian@sudo.site>
2022-12-26 23:32:29 +0100

    uno_arduino fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ source env/bin/activate
 echo "setting up rfcomm0..."
 sudo rfcomm bind 0 00:18:E4:40:00:06
 sudo chmod a+rw /dev/rfcomm0
-echo "setting up uno controller..."
-sudo chmod a+rw /dev/ttyACM0
 echo "setting up encoder controller..."
+sudo chmod a+rw /dev/ttyACM0
+echo "setting up uno controller..."
 sudo chmod a+rw /dev/ttyACM1
