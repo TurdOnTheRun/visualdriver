1d28ed3992cb38d25a029293327bc82c7445a061
Maximilian Weber <maximilian@sudo.site>
2023-05-01 20:32:03 +0200

    uno working. done

--- driver/testing.py
+++ driver/testing.py
@@ -10,17 +10,34 @@ eventDict = {
     'time': []
 }
 
-eventDict['time'].append(ChannelSetChannel(At(0), TopController, Channel1, StaticChannel6))
-eventDict['time'].append(LightSetChannel(At(0), TopController, [Light1, Light2, Light3, Light4], Channel1))
+controllers = [BottomController, TopController]
 
-eventDict['time'].append(EffectUpDownVibrato(At(5), TopController, 0, StaticChannel5, StaticChannel5))
-eventDict['time'].append(ChannelAddEffect(At(5), TopController, Channel1, 0, 0))
+for controller in controllers:
+    eventDict['time'].append(EffectUpDownVibrato(At(0), controller, 0, StaticChannel5, StaticChannel10))
+    # eventDict['time'].append(EffectStrobe(At(0), controller, 1, StaticChannel10, StaticChannel5, 2, [Light1, Light3]))
 
-eventDict['time'].append(EffectStrobe(At(10), TopController, 1, StaticChannel10, StaticChannel5, 2, [Light1, Light3]))
-eventDict['time'].append(ChannelRemoveEffect(At(10), TopController, Channel1, 0))
+    # Strobe Effect
+    eventDict['time'].append(ChannelSetChannel(At(0), controller, Channel2, StaticChannel5))
+    eventDict['time'].append(ChannelAddEffect(At(0), controller, Channel2, 0, 0))
+    eventDict['time'].append(EffectStrobe(At(0), controller, 1, StaticChannel10, Channel2, 1, [Light1, Light3]))
 
-eventDict['time'].append(ChannelAddEffect(At(12), TopController, Channel1, 1, 0))
-eventDict['time'].append(LightSetChannel(At(15), TopController, [Light1, Light2, Light3, Light4], StaticChannel0))
+    eventDict['time'].append(ChannelSetChannel(At(0), controller, Channel1, StaticChannel10))
+    eventDict['time'].append(ChannelAddEffect(At(0), controller, Channel1, 1, 0))
+    eventDict['time'].append(LightSetChannel(At(0), controller, [Light1, Light2, Light3, Light4], Channel1))
+
+    # eventDict['time'].append(ChannelAddEffect(At(2), controller, Channel1, 0, 0))
+    # eventDict['time'].append(ChannelAddEffect(At(5), controller, Channel1, 1, 0))
+
+
+    # eventDict['time'].append(EffectStrobe(At(10), controller, 1, StaticChannel10, StaticChannel5, 2, [Light1, Light3]))
+    # eventDict['time'].append(ChannelRemoveEffect(At(10), controller, Channel1, 0))
+
+    # eventDict['time'].append(ChannelAddEffect(At(12), controller, Channel1, 1, 0))
+
+
+    eventDict['time'].append(LightSetChannel(At(15), controller, [Light1, Light2, Light3, Light4], StaticChannel0))
+
+eventDict['time'] = sorted(eventDict['time'], key=lambda event: event.condition.value)
 
 
 vd = VisualDriver(eventDict, startTime=0)
